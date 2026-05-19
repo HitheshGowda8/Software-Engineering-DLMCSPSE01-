085db2a904a947d191d8818ef01bc9434b39b944
HitheshGowda8 <gowdahithesh817@gmail.com>
2026-05-19 08:42:43 +0200

    Add files via upload

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const {
+  addProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  getLowStockProducts,
+} = require("../controllers/productController");
+
+const router = express.Router();
+
+router.post("/", addProduct);
+router.get("/", getProducts);
+router.get("/low-stock", getLowStockProducts);
+router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
--- backend/routes/saleRoutes.js
+++ backend/routes/saleRoutes.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+const {
+  recordSale,
+  getSales,
+  getSalesReport,
+} = require("../controllers/saleController");
+
+const router = express.Router();
+
+router.post("/", recordSale);
+router.get("/", getSales);
+router.get("/report", getSalesReport);
+
+module.exports = router;
